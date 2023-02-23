69d0df76b8974d43eaf74ba5f1a14ec55f61c59a
Kevin Zhang <kzhang5832@gmail.com>
2023-02-23 02:14:34 -0500

    02232023 darkmode

--- js/index.js
+++ js/index.js
@@ -1,42 +1,68 @@
 window.onload = function() {
-    
-    function isElementInView(element) {
-      var rect = element.getBoundingClientRect();
-      return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-      );
-    }
-    
-    function checkForProjectSection() {
-        var projectSection = document.getElementById('projects');
-        if (!projectSection.classList.contains('show') && isElementInView(projectSection)) {
-        projectSection.classList.add('show');
-        window.removeEventListener('scroll', checkForProjectSection);
-        }
-    }
+  const aboutLink = document.getElementById('about-link');
+  const projectLink = document.getElementById('project-link');
+  const contactLink = document.getElementById('contact-link');
+  const darkModeBtn = document.getElementById('dark-mode-btn');
+  darkModeBtn.addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+  });
   
-    
-    function checkForSkillsSection() {
-      var skillsSection = document.getElementById('skills');
-      if (isElementInView(skillsSection)) {
-        skillsSection.classList.add('show');
-        window.removeEventListener('scroll', checkForSkillsSection);
-      }
-    }
-    
-    function checkForContactSection() {
-      var contactSection = document.getElementById('contact');
-      if (isElementInView(contactSection)) {
-        contactSection.classList.add('show');
-        window.removeEventListener('scroll', checkForContactSection);
-      }
-    }
-    
-    window.addEventListener('scroll', checkForProjectSection);
-    window.addEventListener('scroll', checkForSkillsSection);
-    window.addEventListener('scroll', checkForContactSection);
-  };
   
+  
+
+  aboutLink.addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+  });
+
+  projectLink.addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('projects').scrollIntoView({ behavior: 'smooth' });
+  });
+
+  contactLink.addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+  });
+
+
+  function isElementInView(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function checkForProjectHead() {
+    const projectHead = document.getElementById('projectheader');
+    const projectPair = document.getElementById('projectpairs');
+    if (!projectHead.classList.contains('show') && isElementInView(projectHead)) {
+      projectHead.classList.add('show');  
+      projectPair.classList.add('show');
+      window.removeEventListener('scroll', checkForProjectHead);
+    }
+  }
+
+  function checkForSkillsSection() {
+    const skillsSection = document.getElementById('skills');
+    if (isElementInView(skillsSection)) {
+      skillsSection.classList.add('show');
+      window.removeEventListener('scroll', checkForSkillsSection);
+    }
+  }
+
+  function checkForContactSection() {
+    const contactSection = document.getElementById('contact');
+    if (isElementInView(contactSection)) {
+      contactSection.classList.add('show');
+      window.removeEventListener('scroll', checkForContactSection);
+    }
+  }
+
+  window.addEventListener('scroll', checkForSkillsSection);
+  window.addEventListener('scroll', checkForProjectHead);
+  window.addEventListener('scroll', checkForContactSection);
+};
